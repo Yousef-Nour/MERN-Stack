9244f4138ed240a6bd96bd8c62455da45ac6de70
Yousef-Nour <yousefnour809@gmail.com>
2021-08-17 21:47:24 +0300

    Ninja Assignment

---
+++ Fundamentals/Ninja.js
@@ -0,0 +1,29 @@
+class Ninja{
+    constructor(name, health = 100, speed = 3, strength = 3){
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+    }
+
+    sayName(){
+        console.log("Name : "+this.name);
+    }
+
+    showStats(){
+        console.log(
+            "Name : " +this.name+"\n"
+            +"Strength : "+this.strength+"\n"
+            +"Speed : "+this.speed+"\n"
+            +"Health : "+this.health
+        );
+    }
+
+    drinkSake(){
+        this.health += 10;
+    }
+}
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.drinkSake();
+ninja1.showStats();
